dfba787568619b2e31bce82ac5409e55b3ba8d47
Gregory Losco <gtlosco@gmail.com>
2019-03-12 23:27:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
-  puts "Hello #{name}"
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  puts "Hello #{name} you can speak #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
